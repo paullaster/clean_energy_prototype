5a84e4b583965b1e9dbb3ceed2b691e2e8791b95
paullaster <paullasterokoth98@gmail.com>
2023-04-07 18:02:54 +0300

    add init files of prototype

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <template>
-  <div class="card-size" ref="lineChart"></div>
+  <div>
+    
+  </div>
 </template>
 <script>
-import enargyData from "./db/daily_energy_stat.json"
-import * as echarts from "echarts"
+import {HourlyEnergyData} from './views'
 export default {
-  name: 'App',
-  methods: {
-    drawChart() {
-      const chart = echarts.init(this.$refs.lineChart);
-      option = {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line',
-            smooth: true
-          }
-        ]
-      };
-      chart.setOption(option);
-    },
+  name:'App',
+  components: {
+    HourlyEnergyData,
   }
 }
 </script>
-<style>
-.card-size {
-  width: 70vw;
-  max-width: 80vw;
-  height: 80vh;
-}
-</style>
---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+import HourlyEnergyData from './HourlyEnergyData'
+
+export {
+    HourlyEnergyData,
+}
