5ef29e0895fa12c63ae74888e5bca8d8e311bdee
paullaster <paullasterokoth98@gmail.com>
2023-04-07 17:37:33 +0300

    add init files of prototype

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div ref="lineCahrt">
+    chart to appear here
+  </div>
+</template>
+<script>
+import enargyData from "./db/daily_energy_stat.json"
+export default {
+  name: 'App', 
+
+
+}
+</script>
